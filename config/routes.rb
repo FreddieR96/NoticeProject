9e31cd6329d90fd410ec2ddff5a10f9b2e25cd88
FreddieR96 <33072729+FreddieR96@users.noreply.github.com>
2019-07-21 23:10:25 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,9 @@ get 'search/alltime', to: 'main#alltime', as: :alltime_search
 get 'search/lastmonth', to: 'main#lastmonth', as: :lastmonth_search
 get 'search/lastweek', to: 'main#lastweek', as: :lastweek_search
 get 'search', to: 'main#search', as: :search
-get 'hot', to: 'main#hotnotices', as: :hot
 get 'archive', to: 'main#archive', as: :archive
 resources :notices, except: [:index, :new]
 resources :sessions, only: [:new, :create, :destroy]
-get 'last3', to: 'main#last3'
 get 'searchresults', to: 'main#searchresults'
 get 'addreport/:notice_id/:admin_id/:zetype', to: 'reports#add', as: :add_report
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
